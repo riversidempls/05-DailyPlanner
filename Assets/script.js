03e0738e933b9a54fd12bd846e010a4d677c78fe
C. Donnelly <donn0021@umn.edu>
2022-10-03 17:09:30 -0500

    fixed clock timer interval

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,17 @@
-var date = moment();
-var time = moment();
-var currentTime = date.format('LTS');
 
-var currentDate = date.format('dddd, MMMM Do - Y');
-console.log(currentDate); // "10/02/2022"
+
+
+
+
+//create a function to get the date and time
+function setDateTime () {
+var datetime = moment();
+var currentDate = datetime.format('dddd, MMMM Do - Y');
+//console.log(currentDate); // "10/02/2022"
 document.getElementById('currentDay').innerHTML = currentDate;
-
-//create a function to get the time
-function setTime () {
-var currentTime = date.format('LTS');
-console.log(currentTime);
+var currentTime = datetime.format('LTS');
+//console.log(currentTime);
 document.getElementById('currentTime').innerHTML = currentTime;
 }
-// console.log(currentTime);
-// document.getElementById('currentTime').innerHTML = currentTime;
-
-
-// set interval to run setTime function every second
-window.setInterval(setTime, 1000);
+// Set interval to refresh date and time every second
+setInterval(setDateTime, 1000);
