7fc9d0505ed7488e376fcb1c5da9ffa152d07f65
C. Donnelly <donn0021@umn.edu>
2022-10-04 00:24:09 -0500

    fix 8 o'clock logic

--- Assets/script.js
+++ Assets/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     var hournow = currenthour.hour();
     console.log(hournow);
     if (hournow === 8) { 
-    $("#h8").addClass("row, present");
+        $("#h8").addClass("row, present");
     };
     if (hournow === 9) { 
         $("#h9").addClass("row, present");
@@ -43,7 +43,7 @@ $(document).ready(function(){
 
 });
 
-//finally color thefuture hours future
+//finally color thefuture future hours
 
 $(document).ready(function(){
     var currenthour = moment();
@@ -52,29 +52,32 @@ $(document).ready(function(){
     if (hournow <= 8) { 
         $("#h8,#h9,#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 9) { 
+    if (hournow <= 9) { 
+        $("#h9,#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+    };
+    if (hournow <= 10) { 
         $("#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 10) { 
-        $("#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 11) { 
+        $("#11h,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 11) { 
-        $("#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 12) { 
+        $("#12h,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 12) { 
-        $("#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 13) { 
+        $("#13h,#h14,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 13) { 
-        $("#h14,#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 14) { 
+        $("#14h,#h15,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 14) { 
-        $("#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 15) { 
+        $("#15h,#h16,#h17").addClass("row, future");
     };
-    if (hournow === 15) { 
-        $("#h16,#h17").addClass("row, future");
-    };
-    if (hournow === 16) { 
-        $("#h17").addClass("row, future");
+    if (hournow <= 16) { 
+        $("#16h,#h17").addClass("row, future");
+    };    
+    if (hournow <=17) {
+        $("#17h").addClass("row, future);");
     };
 });
 
