33bfcfe7ca49d4e84d19c37ed112331f3dac0ac3
Conor Donnelly <Conor.Donnelly@state.mn.us>
2022-10-04 12:30:52 -0500

    fixed a couple typos in future element ids

--- Assets/script.js
+++ Assets/script.js
@@ -60,25 +60,25 @@ $(document).ready(function(){
         $("#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 11) { 
-        $("#11h,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+        $("#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 12) { 
-        $("#12h,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+        $("#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 13) { 
-        $("#13h,#h14,#h15,#h16,#h17").addClass("row, future");
+        $("#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 14) { 
-        $("#14h,#h15,#h16,#h17").addClass("row, future");
+        $("#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 15) { 
-        $("#15h,#h16,#h17").addClass("row, future");
+        $("#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow < 16) { 
-        $("#16h,#h17").addClass("row, future");
+        $("#h16,#h17").addClass("row, future");
     };    
     if (hournow < 17) {
-        $("#17h").addClass("row, future");
+        $("#h17").addClass("row, future");
     };
 });
 };
