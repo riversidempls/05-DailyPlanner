456437dfe3731085a85d5550d6a589c899e0682b
C. Donnelly <donn0021@umn.edu>
2022-10-04 00:08:37 -0500

    fixed logic to style correctly after midnite

--- Assets/script.js
+++ Assets/script.js
@@ -49,8 +49,8 @@ $(document).ready(function(){
     var currenthour = moment();
     var hournow = currenthour.hour();
     console.log(hournow);
-    if (hournow === 8) { 
-        $("#h9,#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
+    if (hournow <= 8) { 
+        $("#h8,#h9,#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
     };
     if (hournow === 9) { 
         $("#h10,#h11,#h12,#h13,#h14,#h15,#h16,#h17").addClass("row, future");
